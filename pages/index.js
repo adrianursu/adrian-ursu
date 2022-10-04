fd6b6a0160e11c3185282841ea068fc7e9a5e8de
Ursu Adrian <uursu.adrian@gmail.com>
2022-10-04 18:52:58 +0200

    HeroSection commited

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from "next/head";
-import { AiFillInstagram, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
 import Image from "next/image";
-import adrian from "../public/adrian.jpg";
 import maiestria from "../public/laptop.jpg";
 import chat from "../public/chat-bachelor.jpg";
 import myPaint from "../public/my-paint.jpg";
@@ -12,6 +10,7 @@ import dashboard from "../public/creative-tim.jpg";
 import dogs from "../public/dogs.jpg";
 import magdacesti from "../public/magdacesti.jpg";
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
@@ -24,51 +23,7 @@ export default function Home() {
       <main className="bgLight px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <Navbar />
-          <div className="text-center pb-10 px-15 md:px-10">
-            <h2 className="text-4xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Adrian Ursu
-            </h2>
-            <h3 className="text-lg py-2 dark:text-white md:text-3xl">
-              Front-End Developer
-            </h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl md:py-5">
-              I am an ambitious and progress-focused young frontend developer,
-              fresh graduate in Computer Science. Recently relocated to
-              Copenhagen, looking for new opportunities.
-            </p>
-            <div className="mx-auto rounded-full w-60 h-60 relative overflow-hidden mt-5 md:h-96 md:w-96 lg:h-96 lg:w-96">
-              <Image
-                src={adrian}
-                layout="fill"
-                objectFit="cover"
-                alt="image of Adrian Ursu"
-                priority
-              />
-            </div>
-            <div className="text-5xl flex justify-center gap-16 pt-6 text-gray-600 dark:text-gray-400">
-              <a
-                target="_blank"
-                href="https://www.github.com/adrianursu"
-                rel="noreferrer"
-              >
-                <AiFillGithub className="grow" />
-              </a>
-              <a
-                target="_blank"
-                href="https://www.instagram.com/adrian.ursu8"
-                rel="noreferrer"
-              >
-                <AiFillInstagram className="grow" />
-              </a>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://www.linkedin.com/in/ursu-adrian-9487461b9/"
-              >
-                <AiFillLinkedin className="grow" />
-              </a>
-            </div>
-          </div>
+          <HeroSection />
         </section>
         <section>
           <div>
